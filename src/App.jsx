97c53b46e49f6a49f1cab673c1f839ad017ef432
Julianv19 <messiocampo8@gmail.com>
2026-05-14 12:43:06 -0500

    feat(structure)se creo la estructura base para proyectos con react

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App(){
-  return (
-    <div className= "min-h-screen bg-green-800 felx items-center justify-center">
-    <h1 calssName = "text-white text-4xl font-bold">
-      Con Julian Tailwind V4 funciona full...
-    </h1>
-    </div>
-  )
-}
